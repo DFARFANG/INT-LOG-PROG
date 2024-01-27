0c532794aadf5a3c101f932553756792bbc4ae6e
DFARFANG <157228426+DFARFANG@users.noreply.github.com>
2024-01-27 09:41:09 -0600

    Add files via upload

---
+++ Saludo.py
@@ -0,0 +1,8 @@
+#Saludo
+#Comentario 1... Necesito un pancake
+print(58+90) #Suma
+print(78-41); #Resta
+print(9*12) #Multiplicación
+print(583/22) #División
+print("Que tal terrícolas") #Saludo
+print("Farfán") #Nombre
